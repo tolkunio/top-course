3804f96cc68918099d0eb82979774a8101ebfe27
Tolkun Omurbekova <tolkun3922@gmail.com>
2024-07-11 10:52:40 +0600

    -задан шрифт и цвет

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return<>
+    <Head>
+      <title>Top Course App</title>
+      <link rel="icon" href="/favicon.ico"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap" rel="stylesheet"/>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
